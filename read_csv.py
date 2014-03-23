d4e013c7600b251206890064db8372f7817285c4
dtredger <d.tredger@gmail.com>
2014-03-23 14:23:51 -0400

    use HUMID_LOG env variable if possible

--- read_csv.py
+++ read_csv.py
@@ -5,17 +5,19 @@
 import os
 import glob
 
-print 'yo'
+# called without arguments, get_humidity() will look in the dir indicated by 
+# the HUMID_LOG environment variable
+try:
+	humidity_folder = os.environ.get(HUMID_LOG)
+except:
+	humidity_folder = "/"
 
-def get_humidity(folder):
+
+# returns a line of csv like ['1395513142', ' T 19.2', ' H 31.1\n']
+def get_newest_line(folder=humidity_folder):
 	os.chdir(folder)
 	newest = max(glob.iglob('*.*'), key=os.path.getctime)
 	file = open(newest, 'r')
 	lines = file.readlines()
 	file.close()
-
-	# returns a line of csv like ['1395513142', ' T 19.2', ' H 31.1\n']	
 	return lines[-1].split(',')
-
-def sjso():
-	print 'works'
